84300ceedd8ed28866c4121bc278497114406638
Aksha-max <akshayaseenuvasan@gmail.com>
2024-02-12 13:56:11 +0530

    first cm

---
+++ .vscode/cli/JSON.API/TypeScript/HW.ts
@@ -0,0 +1,9 @@
+class Firstclass{
+    getHello():void{
+        //console.log("Hello world!!!"+(name==undefined?":name"))
+        console.log("Hello World!!!")
+    }
+}
+var obj =  new Firstclass();
+obj.getHello();
+//obj.getHello("Ammu");
---
+++ .vscode/cli/JSON.API/script.js
@@ -0,0 +1,12 @@
+let json1 = "Ammu"
+let json2 = 10
+let json3 =[46,8,10]
+let json5 ={
+    "stock":"TCS",
+    "Price":6810
+}
+let json6 ='[
+     
+     
+]'
+
